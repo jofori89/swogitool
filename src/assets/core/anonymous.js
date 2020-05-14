514e71dfc4ae66a22d0e868586ef49b72d92cc35
jofori89 <jofori89@gmail.com>
2020-05-14 23:44:36 +0700

    Add Code

--- src/assets/core/anonymous.js
+++ src/assets/core/anonymous.js
@@ -11,6 +11,22 @@ function parseBool (str) {
 	return /^true$/i.test(str);
 }
  
+function showBackupImage(elm) {
+    console.error('Image missing: ' + elm.src);
+    elm.src = hostImage + 'default.jpg';
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 /**
  * Clear any potential miss-selection.
  */
